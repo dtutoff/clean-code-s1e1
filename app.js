7df211dff3ae34f080985b2df498633cd140a1d4
Denis Tutov <vrednokot@gmail.com>
2023-04-24 14:34:26 +0300

    refactor: update files to BEM notation according to rule 2.1 in html-and-css-extended.md and 3.1-3.4 html-and-css.md

--- app.js
+++ app.js
@@ -10,7 +10,7 @@
 
 var taskInput=document.getElementById("new-task");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder=document.getElementById("incompletetasks");//ul of #incompletetasks
+var incompleteTaskHolder=document.getElementById("incomplete-tasks");//ul of #incomplete-tasks
 var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
 
 
@@ -33,17 +33,18 @@ var createNewTaskElement=function(taskString){
     var deleteButtonImg=document.createElement("img");//delete button image
 
     label.innerText=taskString;
-    label.className='task';
+    label.className='tasks-list__text';
 
     //Each elements, needs appending
     checkBox.type="checkbox";
+    checkBox.className="tasks-list__marker task-marker";
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="tasks-list__input";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="tasks-list__edit-button edit-button";
 
-    deleteButton.className="delete";
+    deleteButton.className="tasks-list__edit-button delete-button";
     deleteButtonImg.src='./remove.svg';
     deleteButtonImg.alt='delete icon button';
     deleteButton.appendChild(deleteButtonImg);
@@ -83,9 +84,9 @@ var editTask=function(){
 
     var listItem=this.parentNode;
 
-    var editInput=listItem.querySelector('input[type=text]');
-    var label=listItem.querySelector("label");
-    var editBtn=listItem.querySelector(".edit");
+    var editInput=listItem.querySelector('.tasks-list__input');
+    var label=listItem.querySelector(".tasks-list__text");
+    var editBtn=listItem.querySelector(".edit-button");
     var containsClass=listItem.classList.contains("editmode");
     //If class of the parent is .editmode
     if(containsClass){
@@ -132,7 +133,7 @@ var taskIncomplete=function(){
     console.log("Incomplete Task...");
 //Mark task as incomplete.
     //When the checkbox is unchecked
-    //Append the task list item to the #incompletetasks.
+    //Append the task list item to the #incomplete-tasks.
     var listItem=this.parentNode;
     incompleteTaskHolder.appendChild(listItem);
     bindTaskEvents(listItem,taskCompleted);
@@ -156,9 +157,9 @@ addButton.addEventListener("click",ajaxRequest);
 var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
     console.log("bind list item events");
 //select ListItems children
-    var checkBox=taskListItem.querySelector("input[type=checkbox]");
-    var editButton=taskListItem.querySelector("button.edit");
-    var deleteButton=taskListItem.querySelector("button.delete");
+    var checkBox=taskListItem.querySelector(".task-marker");
+    var editButton=taskListItem.querySelector(".edit-button");
+    var deleteButton=taskListItem.querySelector(".delete-button");
 
 
     //Bind editTask to edit button.
